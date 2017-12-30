9ebecd54e9e29294f85c11bc42f395ba7d15caf2
Weera Wu <weera@oozou.com>
2017-12-31 01:40:41 +0700

    Implement map and notification

--- js/app.js
+++ js/app.js
@@ -1,21 +1,67 @@
-var model = {
-
+var Notification = {
+    message: null
 };
 
-var octopus = {
-    init: function() {
-        view.init();
-    }
-};
-
-var view = {
-    init: function() {
-        view.render();
+var Octopus = {
+    init: function () {
+        MapView.init();
+        NotificationView.init();
+        Octopus.notify('Welcome to APP_NAME!');
     },
 
-    render: function() {
-        console.log('view rendered');
+    hasNotificationMessage: function () {
+        return Octopus.getNotificationMessage() !== null;
+    },
+
+    getNotificationMessage: function () {
+        return Notification.message;
+    },
+
+    notify: function (message) {
+        Notification.message = message;
+        NotificationView.render();
+        setTimeout(function () {
+            Notification.message = null;
+            NotificationView.render();
+        }, 3000);
     }
 };
 
-octopus.init();
+var NotificationView = {
+    init: function () {
+        NotificationView.element = $('.notification');
+        NotificationView.render();
+    },
+
+    render: function () {
+        if (Octopus.hasNotificationMessage()) {
+            NotificationView.element.html(
+                '<p>' + Octopus.getNotificationMessage() + '</p>'
+            );
+            NotificationView.element.show();
+        } else {
+            NotificationView.element.hide();
+        }
+    }
+};
+
+var MapView = {
+    init: function () {
+        MapView.render();
+    },
+
+    render: function () {
+        var bangkok = {
+            lat: 13.7248936,
+            lng: 100.493025
+        };
+        var map = new google.maps.Map(
+            $('.map').get(0), {
+                center: bangkok,
+                zoom: 13
+            }
+        );
+    }
+};
+
+Octopus.init();
